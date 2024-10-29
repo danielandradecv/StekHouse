4422a8d05caedfbd9d29b88344f0350218809b0f
Daniel Andrade <danielandradecv@gmail.com>
2024-10-28 21:29:40 -0500

    solamnte creada la estructura de tailwind para su uso y un script build:css ejecutra la renderisacion

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
