8b818eb243bc19705be68cd7d5a96abdb978ce33
Daniel Andrade <danielandradecv@gmail.com>
2024-11-01 21:55:34 -0500

    estructura bien y creadas paginas layouts y componentes

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+
+
+
+export default defineConfig({
+  // Aquí puedes añadir configuraciones adicionales para Astro si las necesitas.
+});
+
+
